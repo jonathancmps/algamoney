0ffdbcdc59f367f970fcd440d13630899967951a
Jonathan Silva Campos <jonathancampos@brq.com>
2021-09-22 19:50:13 -0300

    commit inicial

---
+++ src/main/java/com/example/money/api/MomeyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.money.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomeyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomeyApiApplication.class, args);
+	}
+
+}
